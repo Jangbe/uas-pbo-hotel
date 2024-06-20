9b9e9fc794ed53b43e7a8e7e8efbe1188568843a
citraulia <citraaulia1812@gmail.com>
2024-06-20 11:54:48 +0700

    Update Room

---
+++ src/main/java/com/kelompok1/hotel/model/Room.java
@@ -0,0 +1,66 @@
+package com.kelompok1.hotel.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "rooms")
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roomId;
+
+    @Column(nullable = false)
+    private String roomNo;
+
+    @Column(nullable = false)
+    private String roomType;
+
+    @Column(nullable = false)
+    private String bookingStatus;
+
+    public Room() {
+    }
+
+    public Room(String roomNo, String roomType, String bookingStatus) {
+        this.roomNo = roomNo;
+        this.roomType = roomType;
+        this.bookingStatus = bookingStatus;
+    }
+
+    public Long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Long roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getRoomNo() {
+        return roomNo;
+    }
+
+    public void setRoomNo(String roomNo) {
+        this.roomNo = roomNo;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public String getBookingStatus() {
+        return bookingStatus;
+    }
+
+    public void setBookingStatus(String bookingStatus) {
+        this.bookingStatus = bookingStatus;
+    }
+}
